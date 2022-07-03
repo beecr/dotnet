533ea6ab4bd66bf80dc91045f54fd6e19c70d097
Balakrishnan Changaram Rajagopalan <ITA296@ITA.GOV.OM>
2022-07-03 10:53:12 +0400

    Initial Commit

---
+++ IfElse/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter the proce:");
+
+int price = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (price<15)
+{
+    Console.WriteLine("Buy Now");
+}
+else if (price >=15 && price <18)
+{
+    Console.WriteLine("Bargain!");
+}
+else if (price >= 18 && price < 20)
+{
+    Console.WriteLine("Think about it!");
+}
+else
+{
+    Console.WriteLine("Dont Buy");
+}
